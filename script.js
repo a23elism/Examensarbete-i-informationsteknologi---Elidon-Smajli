fae9f40e6f675c7c5afd257ac0ac671bb64717c4
a23elism <a23elism@student.his.se>
2026-04-06 20:19:50 +0200

    Started Rework on gameboard
    ENG: Rework on gameboard started to actually have logic (to allow game to actually be playable later)
    
    SWE: Omgjort logik för "spelbrädan" så att det faktiskt finns logik (så att spellogik faktiskt kan implementeras)

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ const tileTypes = [
   "orange"
 ];
 
+const boardData = [];
+
 // Creates singular tile/squate
 function createTile(color, row, col) {
   const tile = document.createElement("div");
   tile.classList.add("tile");
   tile.style.backgroundColor = color;
 
-  // Temporary for now (will add more later)
   tile.dataset.row = row;
   tile.dataset.col = col;
 
@@ -25,15 +26,15 @@ function createTile(color, row, col) {
 }
 
 // Creates 8x8 board
-function createBoard() {
+function generateBoardData(){
   for (let row = 0; row < gridSize; row++) {
-    for (let col = 0; col < gridSize; col++) {
-      const randomColor =
-        tileTypes[Math.floor(Math.random() * tileTypes.length)];
+    const currrentRow = [];
 
-      const tile = createTile(randomColor, row, col);
-      board.appendChild(tile);
+    for (let col = 0; col < gridSize; col++) {
+      const randomColor = tileTypes[Math.floor(Math.random() * tileTypes.length)];
+      currrentRow.push(randomColor);
     }
+    boardData.push(currrentRow)
   }
 }
 
